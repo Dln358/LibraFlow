535d7729987842b64d3dc0b8420ee70934b51d01
Dylan Lindeman <dln.devwork@gmail.com>
2024-02-19 16:07:51 -0600

    Starting File for delete functionality

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(cors());
 
 // Create a MySQL connection pool
 const pool = mysql.createPool({
-    host: ' fill in ',
-    user: ' fill in ',
-    password: 'fill in ',
+    host: 'fill in',
+    user: 'fill in',
+    password: 'fill in',
     database: 'libraflow',
 });
 
