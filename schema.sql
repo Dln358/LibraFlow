e7061013f4f35cbe9621470050770db90d213dac
Dylan Lindeman <dln.devwork@gmail.com>
2024-03-04 14:52:25 -0600

    Updating main for newest fixes

--- schema.sql
+++ schema.sql
@@ -12,3 +12,14 @@ CREATE TABLE Books (
     PDFURL VARCHAR(255),
     Description TEXT
 );
+
+-- If you already implemented the previous table, correct it with the following
+ALTER TABLE Books
+    MODIFY ISBN CHAR(13),
+    ADD CONSTRAINT CHK_ISBN (ISBN>1000000000000 AND ISBN<9999999999999);
+
+
+-- Temporary fix i used until further investigation
+ALTER TABLE Books
+    MODIFY ISBN CHAR(13),
+    ADD CONSTRAINT CHK_ISBN CHECK (ISBN REGEXP '^[0-9]+$');
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3002;
 
 // Use CORS middleware
 app.use(cors());
